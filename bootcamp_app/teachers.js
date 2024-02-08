1d836586f3af0e984d26f84de8ba07735a0a5b40
Eric <ericbradshaw98@gmail.com>
2024-02-07 21:27:03 -0500

    injection safety

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,22 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortName = process.argv[2]; 
-const limit = parseInt(process.argv[3]);
+const cohortName = process.argv[2] || 'JUL02'; 
 
 
-pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-ORDER BY teacher;`)
-
+pool.query(`
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+`, [cohortName])
 .then(res => {
-  console.log(res.rows);
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  });
 })
-.catch(err => console.error('query error', err.stack));
 
 
 
